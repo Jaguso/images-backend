cdd0bdf69ab5746aee4788de49b994fe7ddf59c3
Jaguso <jagutzo@gmail.com>
2019-05-05 00:39:54 -0500

    configured mongoDB server (with password as an enviroment variable)g

--- app.js
+++ app.js
@@ -6,11 +6,16 @@ const mongoose = require('mongoose');
 
 const userRoutes = require('./api/routes/user');
 
-// app.use((req, res) => {
-//     res.status(200).json({
-//         message: 'It works'
-//     });
-// });
+mongoose.connect(
+    'mongodb://author:' +
+    process.env.DB_PASSWORD +
+    '@cluster0-shard-00-00-gifns.mongodb.net:27017,cluster0-shard-00-01-gifns.mongodb.net:27017,cluster0-shard-00-02-gifns.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
+    {
+        useNewUrlParser: true,
+        useCreateIndex: true
+    }
+);
+
 
 app.use('/user', userRoutes);
 
