d93eb93ec1fe69b39b381f85f8c98c58a37f7662
Jaguso <jagutzo@gmail.com>
2019-05-06 17:06:49 -0500

    login: checked if passwords coincide (still missing jwt)

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -41,7 +41,40 @@ exports.user_signup = (req, res, next) => {
                 
                 });
             }
+        })  
+}
+
+
+exports.user_login = (req, res, next) => {
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length < 1) {
+                return res.status(401).json({
+                    message: 'Auth failed'
+                });
+            }
+            //next is using bcrypt to compare passwords 
+            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                if (err) {
+                    return res.status(401).json({
+                        message: 'Auth failed'
+                    });
+                }
+                if (result) {
+                    return res.status(200).json({
+                        message: 'Auth successfull'
+                    })
+                }
+                res.status(401).json({
+                    message: 'Auth failed'
+                });
+            });
         })
-       
-    
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                error: err
+            });
+        });
 }
