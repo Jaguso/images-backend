01ed9c87bcbf060c6711957f05e6cc4faa670bef
Jaguso <jagutzo@gmail.com>
2019-05-06 17:33:35 -0500

    finished login (added a jwt)

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -62,8 +63,17 @@ exports.user_login = (req, res, next) => {
                     });
                 }
                 if (result) {
+                    //first parameter defines the information that the jwt encodes
+                    const token = jwt.sign({
+                        email: user[0].email,
+                        userId: user[0]._id
+                        }, 
+                        process.env.JWT_KEY,
+                        { expiresIn: "1h" }
+                    );
                     return res.status(200).json({
-                        message: 'Auth successfull'
+                        message: 'Auth successfull',
+                        token: token
                     })
                 }
                 res.status(401).json({
