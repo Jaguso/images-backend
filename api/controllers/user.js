1b92d41e65cb8e10ab87fa8d8dc9f8c8474733f8
Jaguso <jagutzo@gmail.com>
2019-05-06 16:25:27 -0500

    signup does not allow to repeat emails

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -4,32 +4,44 @@ const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
 exports.user_signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if (err) {
-            return res.status(500).json({
-                error: err
-            });
-        } else {
-            const user = new User({
-                _id: new mongoose.Types.ObjectId(),
-                name: req.body.name,
-                email: req.body.email,
-                password: hash
-            });
-            user.save()
-                .then(result => {
-                    console.log(result) //this prints user json on the console
-                    res.status(201).json({
-                        message: 'User created successfully'
-                    })
-                })
-                .catch(err => {
-                    console.log(err)
-                    res.status(500).json({
-                        error: err
-                    });
-                })
-        }
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length >= 1) {
+                return res.status(409).json({
+                    message: 'Email already exists'
+                });
+            } else {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).json({
+                            error: err
+                        });
+                    } else {
+                        const user = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            name: req.body.name,
+                            email: req.body.email,
+                            password: hash
+                        });
+                        user.save()
+                            .then(result => {
+                                console.log(result) //this prints user json on the console
+                                res.status(201).json({
+                                    message: 'User created successfully'
+                                })
+                            })
+                            .catch(err => {
+                                console.log(err)
+                                res.status(500).json({
+                                    error: err
+                                });
+                            })
+                    }
+                
+                });
+            }
+        })
+       
     
-    });
 }
