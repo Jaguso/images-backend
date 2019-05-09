53e68e874f4edc92cea3dfaa3356df6747604e94
Jaguso <jagutzo@gmail.com>
2019-05-09 16:20:33 -0500

    b

--- api/models/picture.js
+++ api/models/picture.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const pictureSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    picture: { type: String, required: true},
+    picture: { type: String, required: true },
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     description: { type: String }    
 });
