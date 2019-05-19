681a48a8bd83c3bcfc015b3ac80643ec8d3cbfb1
Jaguso <jagutzo@gmail.com>
2019-05-19 18:13:12 -0500

    b

--- app.js
+++ app.js
@@ -14,10 +14,11 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 
-mongoose.connect(
-    'mongodb://author:' +
-    process.env.DB_PASSWORD +
-    '@cluster0-shard-00-00-gifns.mongodb.net:27017,cluster0-shard-00-01-gifns.mongodb.net:27017,cluster0-shard-00-02-gifns.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
+mongoose.connect( 
+    process.env.MONGODB_URI,
+    // 'mongodb://author:' +
+    // process.env.DB_PASSWORD +
+    // '@cluster0-shard-00-00-gifns.mongodb.net:27017,cluster0-shard-00-01-gifns.mongodb.net:27017,cluster0-shard-00-02-gifns.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
     {
         useNewUrlParser: true,
         useCreateIndex: true
