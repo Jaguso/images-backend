205349addf87b9a4758154265256a72c8cb53a16
Jaguso <jagutzo@gmail.com>
2019-05-19 20:56:38 -0500

    b

--- server.js
+++ server.js
@@ -2,31 +2,31 @@ require('dotenv').config();
 const http = require('http');
 
 
-process
-  .on('SIGTERM', shutdown('SIGTERM'))
-  .on('SIGINT', shutdown('SIGINT'))
-  .on('uncaughtException', shutdown('uncaughtException'));
+// process
+//   .on('SIGTERM', shutdown('SIGTERM'))
+//   .on('SIGINT', shutdown('SIGINT'))
+//   .on('uncaughtException', shutdown('uncaughtException'));
 
 // setInterval(console.log.bind(console, 'tick'), 1000);
 // http.createServer((req, res) => res.end('hi'))
 //   .listen(process.env.PORT || 3000, () => console.log('Listening'));
 
-function shutdown(signal) {
-  return (err) => {
-    console.log(`${ signal }...`);
-    if (err) console.error(err.stack || err);
-    setTimeout(() => {
-      console.log('...waited 5s, exiting.');
-      process.exit(err ? 1 : 0);
-    }, 5000).unref();
-  };
-}
+// function shutdown(signal) {
+//   return (err) => {
+//     console.log(`${ signal }...`);
+//     if (err) console.error(err.stack || err);
+//     setTimeout(() => {
+//       console.log('...waited 5s, exiting.');
+//       process.exit(err ? 1 : 0);
+//     }, 5000).unref();
+//   };
+// }
 
 const app = require('./app');
 
 
-// const port = process.env.PORT || 9000;
+const port = process.env.PORT || 9000;
 
 const server = http.createServer(app);
 
-// server.listen(port), console.log(`Works on port ${port}`);
+server.listen(port), console.log(`Works on port ${port}`);
