07e5cf5442d668f2a9023bcd563e19cb15c4ebe2
Jaguso <jagutzo@gmail.com>
2019-05-19 20:42:03 -0500

    b

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
 require('dotenv').config();
 const http = require('http');
+
+const http = require('http');
+
+process
+  .on('SIGTERM', shutdown('SIGTERM'))
+  .on('SIGINT', shutdown('SIGINT'))
+  .on('uncaughtException', shutdown('uncaughtException'));
+
+setInterval(console.log.bind(console, 'tick'), 1000);
+http.createServer((req, res) => res.end('hi'))
+  .listen(process.env.PORT || 3000, () => console.log('Listening'));
+
+function shutdown(signal) {
+  return (err) => {
+    console.log(`${ signal }...`);
+    if (err) console.error(err.stack || err);
+    setTimeout(() => {
+      console.log('...waited 5s, exiting.');
+      process.exit(err ? 1 : 0);
+    }, 5000).unref();
+  };
+}
+
 const app = require('./app');
 
 
